606011fe9f17c436fea3ccd7fd2ecb24877324c0
Bruno <bruno9405@fito.br>
2026-03-17 17:17:30 -0300

    Adicionando o postgres

--- app.py
+++ app.py
@@ -2,20 +2,30 @@ from flask import Flask
 from database import db
 from models import User, Job, Application
 from config import Config
+import os
 from flask_jwt_extended import JWTManager
 from routes.auth_routes import auth_bp
 from routes.job_routes import job_auth
 
 app = Flask(__name__)
 
+
+app.config.from_object(Config)
+
+
+database_url = os.environ.get('DATABASE_URL')
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 jwt = JWTManager(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(job_auth)
-
-app.config.from_object(Config)
-
-db.init_app(app)
 
 with app.app_context():
     db.create_all()
